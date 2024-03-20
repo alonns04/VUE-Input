3588b58a39c8fa7b62544a5750290af7af54c240
alonns04 <claudiogabrielalonso@gmail.com>
2024-03-20 12:53:14 -0300

    prueba con modificacion .js

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script>
+import InputTag from "./components/InputTag.vue";
+
+export default{
+  components:{
+    InputTag,
+  },
+  methods: {
+    handleOnTagsChange(tags) {
+      console.log(tags);
+    },
+  },
+};
+
 </script>
 
 <template>
   <div style="text-align: center;">
 <h1>INPUT CON VUE</h1>
+<inputTag  @onTagsChange="handleOnTagsChange"/>
 </div>
 </template>
 
